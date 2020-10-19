d7fcd86f64965fa09b6b902c974c4209a575c611
annrie <annrie@users.noreply.github.com>
2020-10-19 16:16:17 +0900

    style: tuning singleQuote

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '*.(js|vue)': ['eslint --fix --cache', 'prettier --write'],
-  '*.(vue|htm|html|css|less|sass|scss|styl|stylus|md|js)': [
-    'stylelint --fix --cache',
-    'prettier --write ',
+  "*.(js|vue)": ["eslint --fix --cache", "prettier --write"],
+  "*.(vue|htm|html|css|less|sass|scss|styl|stylus|md)": [
+    "stylelint --fix --cache",
+    "prettier --write ",
   ],
-  '*.(htm|html)': ['htmlhint', 'prettier --write'],
-}
+  "*.(htm|html)": ["htmlhint", "prettier --write"],
+};
